28da4b5089e92bc0fa7e3b07654754a9afb5036e
Kartushin Aleksey <Ilegend2011@gmail.com>
2022-09-11 15:45:51 +0300

    Fix up NotesTableStore persisting

--- src/modules/notes/stores/NotesTableStore.ts
+++ src/modules/notes/stores/NotesTableStore.ts
@@ -1,76 +1,93 @@
 import { types, Instance } from 'mobx-state-tree';
-import { INote } from '../../../interfaces';
+import { isEmpty } from 'lodash';
+
 import { NoteStore } from './NoteStore';
 
-export const NotesTableStore = types.snapshotProcessor(
-  types
-    .model('NotesTableStore', {
-      isCreatingMode: types.boolean,
-      mode: types.enumeration('mode', ['table', 'cards']),
-      selectedToEdit: types.maybe(types.reference(NoteStore)),
-    })
-    .views((self) => ({
-      get isEditingMode() {
-        return !!self.selectedToEdit;
-      },
-      get isTableMode() {
-        return self.mode === 'table';
-      },
-      get isCardsMode() {
-        return self.mode === 'cards';
-      },
-    }))
-    .views((self) => ({
-      get isEnabled() {
-        return self.isCreatingMode || self.isEditingMode;
-      },
-    }))
-    .actions((self) => ({
-      switchOnCreatingMode() {
-        self.selectedToEdit = undefined;
-        self.isCreatingMode = true;
-      },
-      switchOffCreatingMode() {
-        self.isCreatingMode = false;
-      },
-      toggleCreatingMode() {
-        if (self.isCreatingMode) {
-          this.switchOffCreatingMode();
-        } else {
-          this.switchOnCreatingMode();
-        }
-      },
-      setTableMode() {
-        self.mode = 'table';
-      },
-      setCardsMode() {
-        self.mode = 'cards';
-      },
-    }))
-    .actions((self) => ({
-      setEditMode(note: INote) {
-        self.switchOffCreatingMode();
-        self.selectedToEdit = note as Instance<typeof NoteStore>;
-      },
-      resetEditMode() {
-        self.switchOnCreatingMode();
-        self.selectedToEdit = undefined;
-      },
-    })),
-  {
-    preProcessor() {
-      const storedData = localStorage.getItem('NotesTableStore');
-      const { selectedToEdit, ...restoredData } =
-        storedData && JSON.parse(storedData);
+import { INote } from '../../../interfaces';
 
-      return !restoredData
-        ? { isCreatingMode: false, mode: 'table' }
-        : restoredData;
-    },
-    postProcessor(snapshot) {
-      localStorage.setItem('NotesTableStore', JSON.stringify(snapshot));
+interface INotesTableStoreState {
+  mode: 'table' | 'cards';
+  isCreatingMode: boolean;
+}
 
-      return snapshot;
+const NotesTableStoreModel = types
+  .model('NotesTableStore', {
+    isCreatingMode: types.boolean,
+    mode: types.enumeration('mode', ['table', 'cards']),
+    selectedToEdit: types.maybe(types.reference(NoteStore)),
+  })
+  .views((self) => ({
+    get isEditingMode() {
+      return !!self.selectedToEdit;
     },
+    get isTableMode() {
+      return self.mode === 'table';
+    },
+    get isCardsMode() {
+      return self.mode === 'cards';
+    },
+  }))
+  .views((self) => ({
+    get isEnabled() {
+      return self.isCreatingMode || self.isEditingMode;
+    },
+  }))
+  .actions((self) => ({
+    switchOnCreatingMode() {
+      self.selectedToEdit = undefined;
+      self.isCreatingMode = true;
+    },
+    switchOffCreatingMode() {
+      self.isCreatingMode = false;
+    },
+    toggleCreatingMode() {
+      if (self.isCreatingMode) {
+        this.switchOffCreatingMode();
+      } else {
+        this.switchOnCreatingMode();
+      }
+    },
+    setTableMode() {
+      self.mode = 'table';
+    },
+    setCardsMode() {
+      self.mode = 'cards';
+    },
+  }))
+  .actions((self) => ({
+    setEditMode(note: INote) {
+      self.switchOffCreatingMode();
+      self.selectedToEdit = note as Instance<typeof NoteStore>;
+    },
+    resetEditMode() {
+      self.switchOnCreatingMode();
+      self.selectedToEdit = undefined;
+    },
+  }));
+
+export const NotesTableStore = types.snapshotProcessor(NotesTableStoreModel, {
+  preProcessor(initialValue: INotesTableStoreState) {
+    try {
+      const restoredData: {
+        [Property in keyof INotesTableStoreState]: string;
+      } = JSON.parse(localStorage.getItem('NotesTableStore') || '{}');
+
+      return !isEmpty(restoredData)
+        ? {
+            isCreatingMode: restoredData.isCreatingMode === 'true',
+            mode: restoredData.mode as 'table' | 'cards',
+          }
+        : initialValue;
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e);
+      return initialValue;
+    }
   },
-);
+
+  postProcessor(snapshot) {
+    localStorage.setItem('NotesTableStore', JSON.stringify(snapshot));
+
+    return snapshot;
+  },
+});
