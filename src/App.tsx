e86e8eb046ec4f368358f0637f57ba53760cdb00
Alexey Kartushin <Ilegend2011@gmail.com>
2025-03-22 00:03:55 +0300

    Перевести проект на vite, обновить package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
--- src/modules/board/components/NotesTable/NotesTable.tsx
+++ src/modules/board/components/NotesTable/NotesTable.tsx
@@ -19,11 +19,11 @@ import { DateRange } from 'rsuite/esm/DateRangePicker';
 
 import { LineTag } from '../../../common/components';
 import { CreateNoteContainer } from '../../containers/CreateNoteContainer';
+import { useNotifier } from '../../../common/api/hooks/useNotifier';
 
 import styles from './NotesTable.module.scss';
 
 import { IHashTag, INote } from '../../../../interfaces';
-import { useNotifier } from '../../../common/api/hooks/useNotifier';
 
 interface NotesTableProps {
   dateRange: {
@@ -73,201 +73,199 @@ const EmptyPlaceholder = ({ onRefresh }: IEmptyPlaceholderProps) => {
   );
 };
 
-export const NotesTable: React.FC<NotesTableProps> = observer(
-  ({
-    dateRange,
-    error,
-    notes = [],
-    isLoading,
-    isFormEnabled = false,
-    mode = 'table',
+export const NotesTable: React.FC<NotesTableProps> = ({
+  dateRange,
+  error,
+  notes = [],
+  isLoading,
+  isFormEnabled = false,
+  mode = 'table',
 
-    onEdit,
-    onRefresh,
-    onDateChange,
-    onDelete,
-    toggleCreatingMode,
-  }) => {
-    const { showSuccess } = useNotifier();
-    const isTableMode = mode === 'table';
-    const handleDateChange = useCallback(
-      (value: DateRange | null) => {
-        value && onDateChange(value[0], value[1]);
-      },
-      [onDateChange],
-    );
+  onEdit,
+  onRefresh,
+  onDateChange,
+  onDelete,
+  toggleCreatingMode,
+}) => {
+  const { showSuccess } = useNotifier();
+  const isTableMode = mode === 'table';
+  const handleDateChange = useCallback(
+    (value: DateRange | null) => {
+      value && onDateChange(value[0], value[1]);
+    },
+    [onDateChange],
+  );
 
-    return (
-      <div>
-        <div className={styles.actions}>
-          <div className={styles.filters}>
-            <div className={styles.filterItem}>
-              <IconButton
-                icon={<AddOutlineIcon />}
-                // onClick={toggleCreatingMode}
-                onClick={() => showSuccess('Success message')}
-                active={isFormEnabled}
-                circle
-              />
-            </div>
-            <div className={styles.filterItem}>
-              <DateRangePicker
-                value={[dateRange.start, dateRange.end]}
-                onChange={handleDateChange}
-                className={styles.dateBtn}
-              />
-            </div>
-          </div>
-          <div>
+  return (
+    <div>
+      <div className={styles.actions}>
+        <div className={styles.filters}>
+          <div className={styles.filterItem}>
             <IconButton
-              icon={<GridIcon />}
-              appearance="subtle"
-              active={mode === 'cards'}
+              icon={<AddOutlineIcon />}
+              // onClick={toggleCreatingMode}
+              onClick={() => showSuccess('Success message')}
+              active={isFormEnabled}
+              circle
             />
-            <IconButton
-              icon={<MenuIcon />}
-              appearance="subtle"
-              active={isTableMode}
+          </div>
+          <div className={styles.filterItem}>
+            <DateRangePicker
+              value={[dateRange.start, dateRange.end]}
+              onChange={handleDateChange}
+              className={styles.dateBtn}
             />
           </div>
         </div>
-        <div className={cn({ [styles.content]: isFormEnabled })}>
-          {isFormEnabled && (
-            <div className={styles.formWrapper}>
-              <CreateNoteContainer />
-            </div>
-          )}
-
-          <Table
-            height={450}
-            loading={isLoading}
-            renderEmpty={() => {
-              return error ? (
-                <EmptyPlaceholder onRefresh={onRefresh} />
-              ) : (
-                'Not found'
-              );
-            }}
-            data={notes}
-            renderRow={(children, item) => {
-              return item?.dayLabel ? (
-                <div>{item.dayLabel}</div>
-              ) : (
-                <>{children}</>
-              );
-            }}
-            headerHeight={50}
-            className={cn(styles.table, {
-              [styles.tableCreatingMode]: isFormEnabled,
-            })}
-          >
-            <Table.Column flexGrow={2} key="title">
-              <Table.HeaderCell
-                className={styles.headerCell}
-                style={{ padding: '4px 20px', marginRight: '2px' }}
-              >
-                <h3 className={styles.header}>Title</h3>
-              </Table.HeaderCell>
-              <Table.Cell
-                dataKey="title"
-                wordWrap="break-word"
-                style={{ padding: '4px 20px' }}
-              >
-                {({ title }) => {
-                  return <h4 className={styles.title}>{title}</h4>;
-                }}
-              </Table.Cell>
-            </Table.Column>
-            <Table.Column flexGrow={2} key="description">
-              <Table.HeaderCell
-                className={styles.headerCell}
-                style={{ padding: '4px 20px 4px 0' }}
-              >
-                <h3 className={styles.header}>Description</h3>
-              </Table.HeaderCell>
-              <Table.Cell
-                dataKey="description"
-                style={{ padding: '4px 25px 4px 5px' }}
-              />
-            </Table.Column>
-            <Table.Column flexGrow={1} key="created">
-              <Table.HeaderCell
-                className={styles.headerCell}
-                style={{ padding: '4px 20px 4px 0' }}
-              >
-                <h3 className={styles.header}>Date</h3>
-              </Table.HeaderCell>
-              <Table.Cell dataKey="created" style={{ padding: 4 }}>
-                {({ created }) => format(new Date(created), 'd MMM Y')}
-              </Table.Cell>
-            </Table.Column>
-            <Table.Column flexGrow={2} key="hashTags">
-              <Table.HeaderCell
-                className={styles.headerCell}
-                style={{ padding: '4px 20px 4px 0' }}
-              >
-                <h3 className={styles.header}>HashTags</h3>
-              </Table.HeaderCell>
-              <Table.Cell dataKey="hashTags" style={{ padding: 4 }}>
-                {({ hashTags }) => {
-                  return hashTags.map((hashTag: IHashTag) => (
-                    <LineTag
-                      key={hashTag.id}
-                      color={hashTag.color}
-                      text={hashTag.text}
-                    />
-                  ));
-                }}
-              </Table.Cell>
-            </Table.Column>
-            <Table.Column width={60}>
-              <Table.HeaderCell
-                className={styles.headerCell}
-                style={{ padding: '4px 20px 4px 0' }}
-              >
-                <></>
-              </Table.HeaderCell>
-              <Table.Cell dataKey="id" style={{ padding: 4 }}>
-                {(item) => {
-                  return (
-                    <Whisper
-                      placement="autoVerticalStart"
-                      trigger="click"
-                      speaker={({ className, left, top, onClose }, ref) => {
-                        return (
-                          <Popover
-                            ref={ref}
-                            className={className}
-                            style={{ left, top }}
-                            full
-                          >
-                            <Dropdown.Menu onSelect={() => onClose()}>
-                              <Dropdown.Item
-                                eventKey={1}
-                                onSelect={() => onDelete(item.id)}
-                              >
-                                Удалить
-                              </Dropdown.Item>
-                              <Dropdown.Item
-                                onSelect={() => onEdit(item.id)}
-                                eventKey={2}
-                              >
-                                Редактировать
-                              </Dropdown.Item>
-                            </Dropdown.Menu>
-                          </Popover>
-                        );
-                      }}
-                    >
-                      <IconButton appearance="subtle" icon={<MoreIcon />} />
-                    </Whisper>
-                  );
-                }}
-              </Table.Cell>
-            </Table.Column>
-          </Table>
+        <div>
+          <IconButton
+            icon={<GridIcon />}
+            appearance="subtle"
+            active={mode === 'cards'}
+          />
+          <IconButton
+            icon={<MenuIcon />}
+            appearance="subtle"
+            active={isTableMode}
+          />
         </div>
       </div>
-    );
-  },
-);
+      <div className={cn({ [styles.content]: isFormEnabled })}>
+        {isFormEnabled && (
+          <div className={styles.formWrapper}>
+            <CreateNoteContainer />
+          </div>
+        )}
+
+        <Table
+          height={450}
+          loading={isLoading}
+          renderEmpty={() => {
+            return error ? (
+              <EmptyPlaceholder onRefresh={onRefresh} />
+            ) : (
+              'Not found'
+            );
+          }}
+          data={notes}
+          renderRow={(children, item) => {
+            return item?.dayLabel ? (
+              <div>{item.dayLabel}</div>
+            ) : (
+              <>{children}</>
+            );
+          }}
+          headerHeight={50}
+          className={cn(styles.table, {
+            [styles.tableCreatingMode]: isFormEnabled,
+          })}
+        >
+          <Table.Column flexGrow={2} key="title">
+            <Table.HeaderCell
+              className={styles.headerCell}
+              style={{ padding: '4px 20px', marginRight: '2px' }}
+            >
+              <h3 className={styles.header}>Title</h3>
+            </Table.HeaderCell>
+            <Table.Cell
+              dataKey="title"
+              wordWrap="break-word"
+              style={{ padding: '4px 20px' }}
+            >
+              {({ title }) => {
+                return <h4 className={styles.title}>{title}</h4>;
+              }}
+            </Table.Cell>
+          </Table.Column>
+          <Table.Column flexGrow={2} key="description">
+            <Table.HeaderCell
+              className={styles.headerCell}
+              style={{ padding: '4px 20px 4px 0' }}
+            >
+              <h3 className={styles.header}>Description</h3>
+            </Table.HeaderCell>
+            <Table.Cell
+              dataKey="description"
+              style={{ padding: '4px 25px 4px 5px' }}
+            />
+          </Table.Column>
+          <Table.Column flexGrow={1} key="created">
+            <Table.HeaderCell
+              className={styles.headerCell}
+              style={{ padding: '4px 20px 4px 0' }}
+            >
+              <h3 className={styles.header}>Date</h3>
+            </Table.HeaderCell>
+            <Table.Cell dataKey="created" style={{ padding: 4 }}>
+              {({ created }) => format(new Date(created), 'd MMM Y')}
+            </Table.Cell>
+          </Table.Column>
+          <Table.Column flexGrow={2} key="hashTags">
+            <Table.HeaderCell
+              className={styles.headerCell}
+              style={{ padding: '4px 20px 4px 0' }}
+            >
+              <h3 className={styles.header}>HashTags</h3>
+            </Table.HeaderCell>
+            <Table.Cell dataKey="hashTags" style={{ padding: 4 }}>
+              {({ hashTags }) => {
+                return hashTags.map((hashTag: IHashTag) => (
+                  <LineTag
+                    key={hashTag.id}
+                    color={hashTag.color}
+                    text={hashTag.text}
+                  />
+                ));
+              }}
+            </Table.Cell>
+          </Table.Column>
+          <Table.Column width={60}>
+            <Table.HeaderCell
+              className={styles.headerCell}
+              style={{ padding: '4px 20px 4px 0' }}
+            >
+              <></>
+            </Table.HeaderCell>
+            <Table.Cell dataKey="id" style={{ padding: 4 }}>
+              {(item) => {
+                return (
+                  <Whisper
+                    placement="autoVerticalStart"
+                    trigger="click"
+                    speaker={({ className, left, top, onClose }, ref) => {
+                      return (
+                        <Popover
+                          ref={ref}
+                          className={className}
+                          style={{ left, top }}
+                          full
+                        >
+                          <Dropdown.Menu onSelect={() => onClose()}>
+                            <Dropdown.Item
+                              eventKey={1}
+                              onSelect={() => onDelete(item.id)}
+                            >
+                              Удалить
+                            </Dropdown.Item>
+                            <Dropdown.Item
+                              onSelect={() => onEdit(item.id)}
+                              eventKey={2}
+                            >
+                              Редактировать
+                            </Dropdown.Item>
+                          </Dropdown.Menu>
+                        </Popover>
+                      );
+                    }}
+                  >
+                    <IconButton appearance="subtle" icon={<MoreIcon />} />
+                  </Whisper>
+                );
+              }}
+            </Table.Cell>
+          </Table.Column>
+        </Table>
+      </div>
+    </div>
+  );
+};
